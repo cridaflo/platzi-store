ba7307130f77e7dcacc8572b6f80febfc064e460
Arias Briñez Carlos <Carlos.AriasBrinez@carvajal.com>
2020-02-17 10:55:52 -0500

    Termine la lección 36 e implemente el footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,8 +33,11 @@ const routes: Routes = [
   {
     path: 'demo',
     loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)
-  }
-  ,
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule)
+  },
   {
     path: '**',
     loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
