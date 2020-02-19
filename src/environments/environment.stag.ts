d8c7dbe3b0563c22a2b2aeb3e084edd0b7811966
unknown <v25a07@gmail.com>
2020-02-19 18:28:05 -0500

    empece la configuracion de firebase

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url_api: 'https://platzi-store-stag.herokuapp.com/'
+  url_api: 'https://platzi-store-stag.herokuapp.com/',
+  firebaseConfig: {
+    apiKey: 'AIzaSyD2u__X1Ur6JyBrnpGKMdBDqdVLFIoCmFM',
+    authDomain: 'platzi-store-6f3ba.firebaseapp.com',
+    databaseURL: 'https://platzi-store-6f3ba.firebaseio.com',
+    projectId: 'platzi-store-6f3ba',
+    storageBucket: 'platzi-store-6f3ba.appspot.com',
+    messagingSenderId: '893004682163',
+    appId: '1:893004682163:web:2364a8a72a7f5d29f131c6'
+  }
 };
