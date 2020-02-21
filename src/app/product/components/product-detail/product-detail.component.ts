f0c35d840cc810599bbe5a318142b8fffdd45005
Florez Pinillos Christian David <Christian.Florez@carvajal.com>
2020-02-21 14:39:56 -0500

    termine clase 12

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import {ProductsService} from '@core/services/products/products.service';
-import {Product} from '@core/models/product.model';
-
+import { ProductsService } from '@core/services/products/products.service';
+import { Product } from '@core/models/product.model';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
@@ -10,7 +11,7 @@ import {Product} from '@core/models/product.model';
 })
 export class ProductDetailComponent implements OnInit {
 
-  product: Product;
+  product$: Observable<Product>;
 
   constructor(
     private route: ActivatedRoute,
@@ -18,22 +19,14 @@ export class ProductDetailComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-        const id = params.id;
-        // console.log(id);
-        this.fetchProduct(id);
-        // this.product = this.productsService.getProduct(id);
-        // console.log(this.product);
-    });
-
+   this.product$ = this.route.params
+    .pipe(
+      switchMap((params: Params) =>{
+        return this.productsService.getProduct(params.id);
+      })
+    );
   }
-
-  fetchProduct(id: string) {
-    this.productsService.getProduct(id)
-    .subscribe(product => {
-        this.product = product;
-    });
-  }
+  
 
   createProduct() {
       const newProduct: Product = {
