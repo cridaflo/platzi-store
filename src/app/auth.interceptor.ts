7e375caeb93c1cce3fd565184e82c8a986b52c2e
Florez Pinillos Christian David <Christian.Florez@carvajal.com>
2020-02-21 18:03:16 -0500

    voy en la clase 18

---
+++ src/app/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
+import { Observable} from 'rxjs';
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+
+  }
+
+  private addToken(request: HttpRequest<any>){
+    const token = '123';
+    if (true) {
+      request = request.clone();
+    }
+  }
+}
