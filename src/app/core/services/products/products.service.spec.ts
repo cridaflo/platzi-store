36496b6af20ec4a4d6ba7e6f0a7f9bf1f72ac90b
Florez Pinillos Christian David <Christian.Florez@carvajal.com>
2020-02-24 10:02:53 -0500

    termine la lección 21

--- src/app/core/services/products/products.service.spec.ts
+++ src/app/core/services/products/products.service.spec.ts
@@ -1,16 +1,65 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { ProductsService } from './products.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
 
-xdescribe('ProductsService', () => {
+describe('ProductsService', () => {
   let service: ProductsService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ProductsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  
+
+  describe('tests for getAllProducts', () => {
+
+    it('should return products', () => {
+
+        //arrange
+      const expectData = [
+        {
+          id: '1',
+          title: 't1',
+          price: 132131,
+          description: 'alsdkfjlakdj',
+          image: 'img/img.jpg'
+        },
+        {
+          id: '2',
+          title: 't2',
+          price: 132131,
+          description: 'alsdkfjlakdj',
+          image: 'img/img.jpg'
+        }
+      ];
+      let dataError, dataResponse;
+
+      service.getAllProducts()
+      .subscribe(response => {
+        dataResponse = response;
+      }, error => {
+        dataError = error;
+      });
+
+      const req = httpTestingController.expectOne(`${environment.url_api}/products/`);
+      req.flush(expectData);
+      //assert
+      expect(dataResponse.length).toEqual(2);
+      expect(req.request.method).toEqual('GET');
+      expect(dataError).toBeUndefined();
+    });
   });
 });
