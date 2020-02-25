0bc63f4498b9acf28bd6136d0d9690b3f390458b
Florez Pinillos Christian David <Christian.Florez@carvajal.com>
2020-02-25 11:34:45 -0500

    termine la lección 4

--- src/app/admin/components/dates/dates.component.ts
+++ src/app/admin/components/dates/dates.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
+import { addDays, format } from 'date-fns';
 
 @Component({
   selector: 'app-dates',
@@ -8,12 +8,16 @@ import * as moment from 'moment';
 })
 export class DatesComponent implements OnInit {
 
-  fecha;
+  date: string;
+
   constructor() {
-    this.fecha = moment().format( 'YYYY/MMMM/DD');
+    // this.fecha = moment().format( 'YYYY/MMMM/DD');
   }
 
+
   ngOnInit(): void {
+    const addNewDays = addDays(new Date(), 20);
+    this.date = format(addNewDays, 'yyyy/MMMM/dd');
   }
 
 }
